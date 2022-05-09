55cccbc74ccdca2b029b489d41d48a252c83c527
Totoblak90 <tobias.blaksley@hotmail.com>
2022-05-09 10:48:44 -0300

    creación del proyecto

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const res = require('express/lib/response');
+
+var app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+app.listen(1500, () => {
+    console.log('App listening in port 1500')
+})
+
+module.exports = app;
