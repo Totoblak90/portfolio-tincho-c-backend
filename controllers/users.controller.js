db23b71d11df2ed89171da7a85e7fa76e8ae2290
blaksleyn <blaksleyn@gmail.com>
2022-05-10 16:13:13 -0300

    Cambiosss

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -33,6 +33,50 @@ const login = async (req, res, next) => {
   }
 };
 
+const resetPassword = async (req, res, next) => {
+  const { newPassword, oldPassword } = req.body;
+
+  try {
+    const user = await User.findByPk(req.user.id);
+    const isMatch = await bcrypt.compare(oldPassword, user.dataValues.password);
+
+    if (!isMatch) {
+      return next({ status: 400, message: "Invalid old password" });
+    }
+
+    await User.update(
+      { password: newPassword },
+      { where: { email: user.dataValues.email } }
+    );
+
+    res.end();
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateUser = async (req, res, next) => {
+  const { name, lastname, country, photo, account_number, email } =
+    req.body.data;
+
+  const newInfo = {};
+
+  if (name) newInfo.name = name;
+  if (lastname) newInfo.lastname = lastname;
+  if (country) newInfo.country = country;
+  if (photo) newInfo.photo = photo;
+  if (account_number) newInfo.account_number = account_number;
+  if (email) newInfo.email = email;
+
+  try {
+    await User.update(newInfo, { where: { id: req.user.id } }).then((resp) => {
+      res.send("Update successful");
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   login,
 };
