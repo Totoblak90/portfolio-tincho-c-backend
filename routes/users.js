ff8928e520ee936431b9af82256b69535885e6d5
Totoblak90 <tobias.blaksley@hotmail.com>
2022-05-10 16:33:12 -0300

    mas fix en rutas usuarios

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ var router = express.Router();
 const { login, resetPassword } = require('../controllers/users.controller');
 
 /* GET users listing. */
-router.post('/', login);
-router.post('/', resetPassword);
+router.post('/login', login);
+router.post('/reset-password', resetPassword);
 
 module.exports = router;
