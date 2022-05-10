d21dc10ca43c5655520919d3bc046047cb21b632
Totoblak90 <tobias.blaksley@hotmail.com>
2022-05-10 17:37:47 -0300

    saque una validación que no tenía sentido

--- uttilities/middlewareAuth.js
+++ uttilities/middlewareAuth.js
@@ -14,7 +14,6 @@ module.exports = async (req, res, next) => {
   try {
       const decoded = jwt.verify(token, JWT_SECRET);
       let user = await User.findByPk(decoded.user.id);
-      if (decoded.user.id === user.dataValues.id) {
   
         // ningún usuario contiene ese correo
         if (!user) return next({ status: 400, message: "No hay un usuario registrado con esa contraseña" });
@@ -22,12 +21,6 @@ module.exports = async (req, res, next) => {
         req.user = user.dataValues;
   
         next();
-      } else {
-        return res.status(403).json({
-          ok: false,
-          mensaje: 'Token inválido'
-        })
-      }
     } catch (error) {
       res.sendStatus(403).json({
         status: 403,
