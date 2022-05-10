230b3d8d2e8c68e037015ce1668cce8d3a4e8980
blaksleyn <blaksleyn@gmail.com>
2022-05-10 17:22:46 -0300

    Cambiosss

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const auth = require("../uttilities/middlewareAuth");
 
 // Controllers
-const { login, resetPassword } = require('../controllers/users.controller');
+const { login, resetPassword } = require("../controllers/users.controller");
 
 /* GET users listing. */
-router.post('/login', login);
-router.post('/reset-password', resetPassword);
+router.post("/login", login);
+router.post("/reset-password", auth, resetPassword);
 
 module.exports = router;
