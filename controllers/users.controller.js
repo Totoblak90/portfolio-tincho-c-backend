6fa5dccaaa548a588ccd34279d5cf4ba0f5ee51c
blaksleyn <blaksleyn@gmail.com>
2022-05-10 16:22:45 -0300

    Cambiosss

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -12,68 +12,43 @@ const login = async (req, res, next) => {
       message: "Se requiere un usuario o contraseña valido",
     });
   }
-  let user = await User.findOne({ where: { username } });
-
-  if (!user) {
-    return next({
-      status: 400,
-      message: "Este usuario no existe",
-    });
-  }
-  const passwordfinal = await bcrypt.compareSync(
-    password,
-    user.dataValues.password
-  );
-
-  if (!passwordfinal) {
-    return next({
-      status: 400,
-      message: "Contraseña invalida",
-    });
-  }
-};
-
-const resetPassword = async (req, res, next) => {
-  const { newPassword, oldPassword } = req.body;
-
   try {
-    const user = await User.findByPk(req.user.id);
-    const isMatch = await bcrypt.compare(oldPassword, user.dataValues.password);
+    let user = await User.findOne({ where: { username } });
 
-    if (!isMatch) {
-      return next({ status: 400, message: "Invalid old password" });
+    if (!user) {
+      return next({
+        status: 400,
+        message: "Este usuario no existe",
+      });
     }
-
-    await User.update(
-      { password: newPassword },
-      { where: { email: user.dataValues.email } }
+    const passwordfinal = await bcrypt.compareSync(
+      password,
+      user.dataValues.password
     );
 
-    res.end();
-  } catch (error) {
-    next(error);
-  }
-};
-
-const updateUser = async (req, res, next) => {
-  const { name, lastname, country, photo, account_number, email } =
-    req.body.data;
-
-  const newInfo = {};
-
-  if (name) newInfo.name = name;
-  if (lastname) newInfo.lastname = lastname;
-  if (country) newInfo.country = country;
-  if (photo) newInfo.photo = photo;
-  if (account_number) newInfo.account_number = account_number;
-  if (email) newInfo.email = email;
-
-  try {
-    await User.update(newInfo, { where: { id: req.user.id } }).then((resp) => {
-      res.send("Update successful");
-    });
-  } catch (error) {
-    next(error);
+    if (!passwordfinal) {
+      return next({
+        status: 400,
+        message: "Contraseña invalida",
+      });
+    }
+    const payload = {
+      user: { id: user.id },
+    };
+    jwt.sign(
+      payload,
+      JWT_SECRET,
+      {
+        expiresIn: "1500d",
+      },
+      (err, token) => {
+        if (err) throw err;
+        return res.json({ token });
+      }
+    );
+  } catch (err) {
+    // console.log(err)
+    next(err);
   }
 };
 
