3df6764bf5832435561d8b8d3b14daa598ffe11d
Totoblak90 <tobias.blaksley@hotmail.com>
2022-05-10 12:12:55 -0300

    cree los modelos de proyecto y de assets de proyecto

---
+++ Db/models/AssetProyecto.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const AssetProyecto = sequelize.define("asset-proyecto", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+  });
+};
---
+++ Db/models/Proyecto.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const Proyecto = sequelize.define("proyecto", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    image: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }
+  });
+};
