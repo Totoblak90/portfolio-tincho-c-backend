e5c4473bf2b10d645ea62f7411887604f42e6e52
Totoblak90 <tobias.blaksley@hotmail.com>
2022-05-17 00:37:16 -0300

    corrección del tipo de respuestas de galeria controlle

--- controllers/galeria.controller.js
+++ controllers/galeria.controller.js
@@ -10,7 +10,7 @@ const fs = require("fs");
 const getPhotos = async (req, res) => {
   let photos = await Galeria.findAll();
   if (!photos || photos.length === 0) {
-    res.status(400).send("No se encuentran fotos");
+    res.status(400).json({status: 400, message: "No se encontró ninguna foto"})
   }
   res.status(200).json(photos);
 };
@@ -19,6 +19,7 @@ const savePhoto = async (req, res) => {
   if (!req.file) {
     return res.status(400).json({
       error: "No se encontró una imágen",
+      status: 400
     });
   }
 
@@ -31,12 +32,11 @@ const savePhoto = async (req, res) => {
   });
 
   if (saveFile) {
-    return res.json({
-      message: "Imagen guardada correctamente",
-    });
+    return res.send("Imagen guardada correctamente")
   } else {
     res.status(400).json({
       message: "No se pudo guardar la imágen",
+      status: 400
     });
   }
 };
@@ -49,12 +49,8 @@ const deletePhoto = async (req, res) => {
   if (!filename) {
     return res.status(404).json({
       message: "Foto no encontrada",
+      status: 404
     });
-  }
-
-  // Borro el archivo físico de la carpeta
-  if (fs.existsSync(`public/gallery/${filename}`)) {
-    fs.unlinkSync(`public/gallery/${filename}`);
   }
 
   // Borro el archivo de la base de datos
@@ -66,12 +62,16 @@ const deletePhoto = async (req, res) => {
   });
 
   if (deletePhoto) {
-    return res.status(200).json({
-      message: "Foto eliminada correctamente",
-    });
+    // Borro el archivo físico de la carpeta
+    if (fs.existsSync(`public/gallery/${filename}`)) {
+      fs.unlinkSync(`public/gallery/${filename}`);
+    }
+
+    return res.status(200).send("Foto eliminada correctamente")
   } else {
     return res.status(400).json({
       message: "No se pudo eliminar la foto",
+      status: 400
     });
   }
 };
